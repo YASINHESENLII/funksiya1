91bf227b1c6c756a4f9c8cad238db2c590f058ca
YASINHESENLII <yasinhesenli44@gmail.com>
2023-03-15 03:28:49 +0400

    Function A

---
+++ Funksiya1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Yasincode
+
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            ReverseNumber(123456);
+        }
+        public static string ReverseNumber(int value)
+        {
+            string textValue = value.ToString().TrimStart();
+            char[] valueChars = textValue.ToCharArray();
+            Array.Reverse(valueChars);
+            string reversedValue = new string(valueChars);
+            Console.WriteLine(reversedValue);
+            return reversedValue;
+        }
+    }
+}
